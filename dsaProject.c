2cecd1dbfc08b2c4e40a17c7270ebdf08ef853cc
abishek982001 <abishek982001@gmail.com>
2020-05-31 16:13:56 +0530

    corrections and improved for n matrices

--- dsaProject.c
+++ dsaProject.c
@@ -19,16 +19,17 @@ int count_lines()
 }
 void PrintParenthesis(int i, int j, int n, int *bracket)
 {
-    static char Mname='A';
+    static int ind=1;
     if(i==j)
     {
-        printf("%c",Mname++);
-         return;
+        printf("A");
+        printf("%d",ind++);
+        return;
     }
     printf("(");
-     PrintParenthesis(i,*((bracket+i*n)+j),n,bracket);
+    PrintParenthesis(i,*((bracket+i*n)+j),n,bracket);
     PrintParenthesis(*((bracket+i*n)+j)+1,j,n,bracket);
-     printf(")");
+    printf(")");
 }
 void MCMO(int p[], int n)
 {
@@ -37,7 +38,8 @@ void MCMO(int p[], int n)
     //memset(M,0,n*sizeof(M[0][0]));
     int bracket[n][n];
     for(int i=1;i<n;i++)
-        M[i][i]=0;
+       { M[i][i]=0;
+        bracket[i][i]=0;}
     for(int len=2;len<n;len++)
     {
         for(int i=1;i<n-len+1;i++)
@@ -45,17 +47,17 @@ void MCMO(int p[], int n)
             int j=i+len-1; M[i][j]=INT_MAX;
             for(int k=i;k<=j-1;k++)
             {
-                int cost=M[i][k]+M[k+1][j]+p[i-1]*p[k]*p[j];
-                printf("cost: %d ,M[%d][%d]: %d,k: %d \n",cost,i,j,M[i][j],k);
+                long long int cost=M[i][k]+M[k+1][j]+p[i-1]*p[k]*p[j];
                  if(cost<M[i][j])
                 {
 
                     bracket[i][j]=k;
-                    printf("since cost :%d < M[%d][%d] :%d, bracket[%d][%d] : %d \n",cost,i,j,M[i][j],i,j,k);
                      M[i][j]=cost;
                 }
+                printf("The cost for M[%d][%d] is %lld at k equal to %d\n",i,j,cost,k);
             }
         }
+        printf("\n");
     }
     printf("\nThe Cost-Matrix is given by:\n");
      for(int i=1;i<n;i++)
@@ -66,7 +68,21 @@ void MCMO(int p[], int n)
             if(i>j)
                 printf("X\t");
             else
-                printf("%d\t",M[i][j]); //space
+                printf("%lld\t",M[i][j]); //space
+
+        }
+        printf("\n"); //bracket
+    }
+    printf("Optimal Paranthesization occurs at \n");
+    for(int i=1;i<n;i++)
+    {
+        for(int j=1;j<n;j++)
+        {
+
+            if(i>j)
+                printf("X\t");
+            else
+                printf("%d\t",bracket[i][j]); //space
 
         }
         printf("\n"); //bracket
@@ -80,11 +96,6 @@ int main()
     FILE *fptr;
     fptr = fopen("input.txt", "r");
     int n=count_lines();
-    if(n>=28)
-    {
-        printf("Total number of inputs can't exceed 27\nDelete some of the inputs in the file");
-        exit(0);
-    }
     int arr[n];
     for(int i=0;i<n;i++)
         fscanf(fptr, "%d", &arr[i]);
