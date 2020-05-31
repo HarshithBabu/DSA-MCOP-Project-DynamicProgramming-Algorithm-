82ec22954876af9109e0b7d5f9c4cb2c41947f58
HarshithBabu <krishnaharshith321@gmail.com>
2020-05-31 14:27:43 +0530

    small updates

--- dsaProject.c
+++ dsaProject.c
@@ -1,26 +1,28 @@
 
-#include<iostream>
-#include<conio.h>
-using namespace std;
-void PrintParenthesis(int i, int j, int n, int *bracket, char &Mname)
-{
+#include<stdio.h>
+#include<limits.h>
+#include<stdlib.h>
+void PrintParenthesis(int i, int j, int n, int *bracket)
+{   
+    static char Mname='A';
     if(i==j)
     {
         printf("%c",Mname++);
         return;
     }
-    printf"(";
-    PrintParenthesis(i,*((bracket+i*n)+j),n,bracket,Mname);
-    PrintParenthesis(*((bracket+i*n)+j)+1,j,n,bracket,Mname);
-    printf")";
+    printf("(");
+    PrintParenthesis(i,*((bracket+i*n)+j),n,bracket);
+    PrintParenthesis(*((bracket+i*n)+j)+1,j,n,bracket);
+    printf(")");
 }
 void MCMO(int p[], int n)
 {
-    int M[n][n];
+    long long int M[n][n];
     int bracket[n][n];
     for(int i=1;i<n;i++)
     {
         M[i][i]=0;
+	bracket[i][i]=0;
     }
     for(int len=2;len<n;len++)
     {
@@ -30,14 +32,16 @@ void MCMO(int p[], int n)
             M[i][j]=INT_MAX;
             for(int k=i;k<=j-1;k++)
             {
-               int cost=M[i][k]+M[k+1][j]+p[i-1]*p[k]*p[j];
+               long long int cost=M[i][k]+M[k+1][j]+p[i-1]*p[k]*p[j];
                if(cost<M[i][j])
                {
                   M[i][j]=cost;
                   bracket[i][j]=k;
                }
+	       printf("The cost of M[%d][%d] is %lld at k equal to %d \n",i,j,cost);
             }
         }
+	printf("\n");
     }
 
     printf("\nThe Cost-Matrix is given by:\n");
@@ -48,14 +52,14 @@ void MCMO(int p[], int n)
             if(i>j)
                 printf("X\t");
             else
-                printf("%d \t",M[i][j]);
+                printf("%lld\t",M[i][j]);
         }
         printf("\n");
     }
     printf("\nOptimal Cost:%d",M[1][n-1]);
     printf("Optimal Parenthesization:");
     char Mname='A';
-    PrintParenthesis(1,n-1,n,(int *)bracket,Mname);
+    PrintParenthesis(1,n-1,n,(int *)bracket);
 }
 int main()
 {
@@ -64,11 +68,10 @@ int main()
     scanf("%d",&n);
     int arr[n+1];
     printf("Enter dimensions of the matrices:\n");
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n+1;i++) 
     {
-       scnaf("%d",arr[i]);
-       MCMO(arr,n+1);
-       getch();
-       return 0;
+        scanf("%d",&arr[i]); 
     }
+      MCMO(arr,n+1);
+      return 0;
 }
